f6d315f27352907d6f2c988fc9c696d827586ff6
Aleksander Kołata <olek.tbg.konta@gmail.com>
2017-11-01 15:18:10 +0100

    navigation bar added

---
+++ src/main/java/com/meetingorganizer/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.meetingorganizer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Home page controller
+ * @author Aleksander
+ */
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    public static final String HOME_PAGE = "homePage";
+
+    @GetMapping
+    public String showHomePage() {
+        return HOME_PAGE;
+    }
+}
